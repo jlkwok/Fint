c7ad5e167e1a081d6529d4df13300379d7754911
Jessica Kwok <jlk187@case.edu>
2019-10-31 14:06:24 -0400

    fields added

--- FrontEnd/src/app/item/item.component.ts
+++ FrontEnd/src/app/item/item.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   currentRate = 3.45;
+  seller: String;
+  title: String;
+  totalNumFints: number;
+  price: number;
+  itemImages: String[];
+
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
   constructor() { }
 
   ngOnInit() {
+    this.seller = "Seller Name";
+    this.title = "Item Title";
+    this.totalNumFints = 3;
+    this.price = 7.89;
+    this.itemImages = ["../../assets/placeholder.png", "../../assets/avatar.png"];
   }
 
 }
