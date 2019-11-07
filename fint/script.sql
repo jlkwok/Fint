7b3e4ed616231b037e37db872069cf6e5bb527a5
Jessica Kwok <jlk187@case.edu>
2019-11-07 15:01:04 -0500

    item and reviewIds objects

--- fint/script.sql
+++ fint/script.sql
@@ -1,2 +1,2 @@
 INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) 
-VALUES ('tuxedo', 20, 'tuxedo.png', 3, true, 'Cleveland, OH', 1);
+VALUES ('Tuxedo', 20, 'tuxedo.png', 3, true, 'Cleveland, OH', 1);
