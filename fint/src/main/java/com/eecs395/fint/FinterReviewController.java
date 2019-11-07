09da0b733076a2715f83470fd755e792921d5a07
kboxeth <knb37@case.edu>
2019-11-06 21:41:30 -0500

    Cross origin annotation to remove cors blocking issues

--- fint/src/main/java/com/eecs395/fint/FinterReviewController.java
+++ fint/src/main/java/com/eecs395/fint/FinterReviewController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@CrossOrigin
 @Controller
 @RequestMapping(path="/finterreview")
 public class FinterReviewController {
