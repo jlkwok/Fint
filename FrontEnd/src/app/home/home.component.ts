3c65fd7778cda17a68c5f5f8ab3320125401b1aa
Jessica Kwok <jlk187@case.edu>
2019-11-19 14:46:13 -0500

    pictures on item cards

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -8,12 +8,12 @@ import { ItemService } from '../shared/services/item.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  //items: Item[];
+  items: Item[];
 
   constructor(private itemService: ItemService) { }
 
   ngOnInit() {
-    //this.itemService.getAllItems().subscribe(items => this.items = items);
+    this.itemService.getAllItems().subscribe(items => this.items = items);
   }
 
 }
--- FrontEnd/src/app/item-card/item-card.component.ts
+++ FrontEnd/src/app/item-card/item-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ItemService } from '../shared/services/item.service';
 import { UserService } from '../shared/services/user.service';
 import { ItemReviewService } from '../shared/services/item-review.service';
+import { Item } from '../shared/models/item';
 
 @Component({
   selector: 'app-item-card',
@@ -9,26 +10,24 @@ import { ItemReviewService } from '../shared/services/item-review.service';
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent implements OnInit {
+  @Input() item: Item;
   rating: number;
   sellerName: String;
   name: String;
   price: number;
   itemImages: String[];
   itemReviewCount: number;
+  image: String;
 
-  image = [1].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-  
-  constructor(private itemService: ItemService, private userService: UserService, private itemReviewService: ItemReviewService) { }
+  constructor(private userService: UserService, private itemReviewService: ItemReviewService) { }
 
   ngOnInit() {
-    this.itemImages = ["../../assets/placeholder.png", "../../assets/avatar.png"];
-    this.itemService.getItem(1).subscribe(item => {
-      this.name = item.name;
-      this.price = item.price;
-      this.userService.getUser(item.finterId).subscribe(seller => this.sellerName = seller.name);
-    });
-    this.itemReviewService.getItemRating(1).subscribe(rating => this.rating = rating);
-    this.itemReviewService.getReviewCount(1).subscribe(itemReviewCount => this.itemReviewCount = itemReviewCount);
+    this.image = "../../assets/" + this.item.picture;
+    this.name = this.item.name;
+    this.price = this.item.price;
+    this.userService.getUser(this.item.finterId).subscribe(seller => this.sellerName = seller.name);
+    this.itemReviewService.getItemRating(this.item.itemId).subscribe(rating => this.rating = rating);
+    this.itemReviewService.getReviewCount(this.item.itemId).subscribe(itemReviewCount => this.itemReviewCount = itemReviewCount);
   }
 
 }
