4f680fd8fa1f04aae120734745234ea398e8da48
kboxeth <knb37@case.edu>
2019-11-26 12:53:31 -0500

    Merge

--- fint/script.sql
+++ fint/script.sql
@@ -7,7 +7,6 @@ INSERT INTO fint.user (name, password, username, location) VALUES ('Matthew', 'M
 INSERT INTO fint.user (name, password, username, location) VALUES ('Louie', 'LouiePassword', 'LouieUsername', 'New York, NY');
 
 INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Tuxedo', 20, 'tuxedo.png', 3, true, 'Cleveland, OH', 1);
-<<<<<<< HEAD
 INSERT INTO fint.user (name, password, username) VALUES ('Kyle the Seller', 'KylePassword', 'KyleUsername');
 INSERT INTO fint.user (name, password, username) VALUES ('Jason the Reviewer', 'JasonPassword', 'JasonUsername');
 INSERT INTO fint.user (name, password, username) VALUES ('Jess the Reviewer', 'JessPassword', 'JessUsername');
@@ -23,12 +22,3 @@ INSERT INTO fint.user (name, password, username) VALUES ('Louie', 'LouiePassword
 INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Tractor', 276, 'tractor.png', 1, true, 'Saline, MI', 7);
 INSERT INTO fint.cart_item (item_id, fintee_id, start_date, end_date, price) VALUES (1, 1, '7 11 2019', '11 14 2019');
 INSERT INTO fint.Transaction (item_id, is_returned, fintee_id, start_date, end_date, t_price) VALUES (1, true, 2, '1 12 2019', '7 12 2019', 100);
-=======
-INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Car', 300, 'car.png', 120, true, 'Akron, OH', 4);
-INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Basketball', 5, 'bball.png', 15, true, 'Seattle, WA', 5);
-INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Grill', 10, 'grill.png', 13, true, 'Redmond, WA', 6);
-INSERT INTO fint.item (name, price, picture, fint_count, is_available, location, finter_id) VALUES ('Tractor', 276, 'tractor.png', 1, true, 'Saline, MI', 7);
-
-INSERT INTO fint.item_review (reviewed_id, reviewer_id, description, rating, post_date) VALUES (1, 2, 'This tuxedo is awesome', 4, '2019-11-07T16:24:29.000');
-INSERT INTO fint.item_review (reviewed_id, reviewer_id, description, rating, post_date) VALUES (1, 3, 'This tuxedo is terrible', 1, '2019-11-07T16:24:29.000');
->>>>>>> 74d249f84c6eb166b65f0db024f20140df8f91bd
